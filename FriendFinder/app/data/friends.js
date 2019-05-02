8126ef89ea080df2e4cdf86a5ff95ec9d14080f4
Jose Torres <jt227b@gmail.com>
2019-05-02 00:40:17 -0500

    Updated Friends.js

---
+++ FriendFinder/app/data/friends.js
@@ -0,0 +1,166 @@
+Friends = [
+    {
+        name: "Jose Torres",
+        photo: "https://jt227b.github.io/Portfolio/assets/image/profile.png",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Onix Castro",
+        photo: "https://onix-xcc.github.io/updatedPortfolioOXCC/assets/images/face.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "JLO",
+        photo: "https://specials-images.forbesimg.com/imageserve/5b43baeca7ea4341970e0eca/416x416.jpg?background=000000&cropX1=302&cropX2=2965&cropY1=19&cropY2=2682",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            5,
+            4,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Florida Man",
+        photo: "https://www.wsbradio.com/rf/image_lowres/Pub/p9/CmgSharedContent/2018/11/15/Videos/4531275.vpx",
+        scores: [
+
+            5,
+            4,
+            1,
+            2,
+            3,
+            4,
+            5,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Cardi B",
+        photo: "https://www.rollingstone.com/wp-content/uploads/2017/10/cardi-b-rolling-stone-interview-cover_.jpg?crop=900:600&width=440",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            1
+        ]
+    },
+    {
+        name: "Ricky Martin",
+        photo: "https://fidelitydiary.com/wp-content/uploads/2019/01/Ricky-Martin.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            4,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Trump",
+        photo: "https://www.gannett-cdn.com/presto/2018/12/19/USAT/aeb39ce2-3c72-4653-8f31-900ad2bcbd3f-AP_Trump.JPG?crop=5471,3077,x0,y382&width=3200&height=1680&fit=bounds",
+        scores: [
+            1,
+            2,
+            1,
+            3,
+            1,
+            2,
+            1,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Florida Women",
+        photo: "https://pbs.twimg.com/profile_images/3383187023/5f8ffc11777825ffc5303a620a7a0756_400x400.jpeg",
+        scores: [
+            1,
+            2,
+            1,
+            2,
+            1,
+            1,
+            1,
+            3,
+            2,
+            1
+        ]
+    },
+    {
+        name: "John Doe",
+        photo: "https://loremflickr.com/640/360",
+        scores: [
+            5,
+            2,
+            1,
+            4,
+            3,
+            4,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Lil Uzi",
+        photo: "https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2019%2F01%2Flil-uzi-vert-done-making-music-0.jpg?fit=max&cbr=1&q=90&w=750&h=500",
+        scores: [
+            1,
+            2,
+            1,
+            3,
+            1,
+            3,
+            1,
+            2,
+            1,
+            1
+        ]
+    }
+]
+
+
+module.exports = Friends;
